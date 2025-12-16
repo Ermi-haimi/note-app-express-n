18a14e5483ea63bd4a47fa29902df8c318e4bfd2
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-12-16 20:18:45 +0300

    added delete and getbyid controllers

--- controller/noteController.js
+++ controller/noteController.js
@@ -3,7 +3,10 @@ import noteModel from "../models/noteModel.js";
 export const getNotes = async (req, res, next)=>{
     try{
         const notes = await noteModel.find().sort({createdAt:-1});
-        res.status(200).json({success: true, count: notes.length, data: notes})
+        res.status(200).json({
+            success: true, 
+            count: notes.length, 
+            data: notes})
     }catch(err){
         next(err);
     }
@@ -12,7 +15,10 @@ export const getNotes = async (req, res, next)=>{
 export const createNote = async(req, res, next)=>{
     try {
         const note = await noteModel.create(req.body);
-        res.status(201).json({success: true, data:note})
+        res.status(201).json({
+            success: true, 
+            data:note
+        })
     } catch (err) {
         next(err)
     }
@@ -25,10 +31,58 @@ export const updateNote = async (req, res, next)=>{
             runValidators: true,
         })
         if(!note){
-            return res.status(404).json({success:false, message:"Note not found"});
+            return res.status(404).json({
+                success:false, 
+                message:"Note not found"
+            });
         }
-        res.status(200).json({success:true, data:note})
+        res.status(200).json({
+            success:true, 
+            data:note
+        })
     }catch(err){
         next(err)
     }
 }
+
+export const getNoteById = async (req, res, next) => {
+    try {
+        const note = await noteModel.findById(req.params.id);
+
+        if (!note) {
+            return res.status(404).json({
+                success: false,
+                message: "Note not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: note,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+
+export const deleteNote = async (req, res, next) => {
+    try {
+        const note = await noteModel.findByIdAndDelete(req.params.id);
+
+        if (!note) {
+            return res.status(404).json({
+                success: false,
+                message: "Note not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Note deleted successfully",
+        });
+    } catch (err) {
+        next(err);
+    }
+};
--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,9 +1,9 @@
 import  express from 'express';
-import { getNotes, createNote, updateNote } from '../controller/noteController.js';
+import { getNotes, createNote, updateNote, getNoteById, deleteNote } from '../controller/noteController.js';
 
 const router = express.Router()
 
 router.route("/").get(getNotes).post(createNote);
-router.route("/:id").patch(updateNote);
+router.route("/:id").patch(updateNote).get(getNoteById).delete(deleteNote);
 
 export default router;
